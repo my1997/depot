fd34406667b21334e9ce5a90c2cb93c9623795dc
unknown <1297573670@qq.com>
2013-07-12 00:04:46 +0800

    add my phase1 depot

---
+++ depot/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '889ba989308fc78fe89a6c20d422ba42da336b06e026e2af9003968be4278d6addf85159ff213996bdad164c24a7af2f1489bf3aeefbeadb0e7018775ef57269'
